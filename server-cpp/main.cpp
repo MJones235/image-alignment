4dc3bc3097f42648ac90fff3e4f0d2cc31bcaed8
Michael Jones <235.michael.jones@gmail.com>
2022-09-14 00:26:28 +0100

    extract string from json request

--- server-cpp/main.cpp
+++ server-cpp/main.cpp
@@ -1,15 +1,24 @@
 #include "crow.h"
+#include <cstdio>
+#include <iostream>
 
 int main() {
-    crow::SimpleApp app;
+  crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/").methods("POST"_method)
-    ([](const crow::request &req)
-     {
-         auto data = crow::json::load(req.body);
-         if (!data)
-             return crow::response(400);
-     });
+  CROW_ROUTE(app, "/").methods("POST"_method)([](const crow::request &req) {
+    auto data = crow::json::load(req.body);
+    if (!data) {
+      return crow::response(400, "Missing request body");
+    } else if (!data.has("image")) {
+      return crow::response(400, "No image provided");
+    } else if (!data.has("template")) {
+      return crow::response(400, "No template provided");
+    } else {
+      auto image = std::string(data["image"]);
+      auto template_image = std::string(data["template"]);
+      return crow::response(200, image);
+    }
+  });
 
-    app.port(18080).multithreaded().run();
+  app.port(18080).multithreaded().run();
 }
